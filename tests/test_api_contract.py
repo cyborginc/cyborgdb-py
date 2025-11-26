444a3a52a635ec1e6c2f6ee2785c3ed993a03603
Kevin Kopczynski <kkopczynski@cyborg.co>
2025-11-26 12:14:46 -0500

    add edge cases to generate_key tests

--- tests/test_api_contract.py
+++ tests/test_api_contract.py
@@ -193,6 +193,19 @@ class TestAPIContract(unittest.TestCase):
         key2 = cyborgdb.Client.generate_key(save=True)
         self.assertEqual(key, key2, "Keys generated with save=True should be the same")
 
+        # Test that it rejects unexpected arguments
+        with self.assertRaises(TypeError):
+            cyborgdb.Client.generate_key(unexpected_arg=123)
+
+        # Test that explicit save=False generates a new key
+        new_key = cyborgdb.Client.generate_key(save=False)
+        self.assertNotEqual(new_key, key, "Keys generated with save=False should be different")
+
+        # Test that member function is able to recall the same key
+        client = cyborgdb.Client(base_url=self.base_url, api_key=self.api_key)
+        member_key = client.generate_key(save=True)
+        self.assertEqual(member_key, key, "Keys generated with save=True should be the same")
+
         # Test that is also works as a member function
         client = cyborgdb.Client(base_url=self.base_url, api_key=self.api_key)
         key = client.generate_key()
