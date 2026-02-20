005a76b8571e6450f9946c8ef95ca4e3430b1d12
Kevin Kopczynski <kkopczynski@cyborg.co>
2026-02-20 15:50:52 -0500

    add __version__ function (#61)
    * add __version__ function
    
    * linter

--- cyborgdb/__init__.py
+++ cyborgdb/__init__.py
@@ -17,6 +17,8 @@ from .client.encrypted_index import EncryptedIndex
 # Re-export demo functionality
 from .demo import get_demo_api_key
 
+from importlib.metadata import PackageNotFoundError, version
+
 # Try to import LangChain integration (optional dependency)
 try:
     from .integrations.langchain import CyborgVectorStore
@@ -36,6 +38,11 @@ except ImportError:
             )
 
 
+try:
+    __version__ = version("cyborgdb-service")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
+
 __all__ = [
     "Client",
     "EncryptedIndex",
@@ -45,4 +52,5 @@ __all__ = [
     "IndexIVFSQ",
     "CyborgVectorStore",
     "get_demo_api_key",
+    "__version__",
 ]
