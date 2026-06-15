0de3cbfa1edadf063ac0c46a555f11f781ecc866
Truman Nguyen <trumanngny@yahoo.com>
2026-06-15 12:55:32 -0400

    fix threshold to match with core (#91)
    Co-authored-by: “Truman <“trumanngny@yahoo.com”>

--- tests/quick_flow_test.py
+++ tests/quick_flow_test.py
@@ -505,7 +505,7 @@ class TestUnitFlow(unittest.TestCase):
             94.04,  # Query #12
             90.05,  # Query #13
             22.00,  # Query #14
-            5.25,  # Query #15
+            3.7,  # Query #15
             70.00,  # Query #16
             70.00,  # Query #17
         ]
